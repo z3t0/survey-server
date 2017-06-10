2e8e8b34e1c805c0f89e9013622409185d527383
Rafi Khan <zetoslab@gmail.com>
2017-06-09 20:50:36 -0600

    added models

--- survey/admin.py
+++ survey/admin.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Question, Survey, Category
+
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Category)
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 3
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    fields = ['name', 'author', 'description', 'date', 'category']
+
+    inlines = [QuestionInline]
+
+
+admin.site.register(Survey, SurveyAdmin)
--- survey/models.py
+++ survey/models.py
@@ -1,6 +1,58 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
+
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category (models.Model):
+    name = models.CharField(max_length=30, blank=True, null=True, default='undefined')
+
+    def __str__(self):
+        if self.name:
+            return self.name
+
+
+class Survey (models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, null=True)
+    description = models.CharField(max_length=300, blank=True)
+    date = models.DateField(default=timezone.now)
+    category = models.ForeignKey(Category, blank=True, null=True)
+
+    def get_questions(self):
+        if self.pk:
+            return Question.objects.filter(survey=self.pk)
+        else:
+            return None
+
+    def __str__(self):
+        return self.name
+
+
+class Question (models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300, blank=True)
+    survey = models.ForeignKey(Survey, null=True, blank=True)
+    required = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class QuestionText(Question):
+    type = models.CharField(max_length=30, default='text')
+
+
+class QuestionCheckBox(Question):
+    type = models.CharField(max_length=30, default='checkbox')
+
+
+class QuestionDropDown(Question):
+    type = models.CharField(max_length=30, default='dropdown')
+
+
+class QuestionMultipleOption(models.Model):
+    name = models.CharField(max_length=30)
+    question = models.ForeignKey(Question, null=True)
