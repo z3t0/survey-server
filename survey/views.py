a9394c0577f3f321d46d034cc61c2bd29a3bc5a0
Rafi Khan <zetoslab@gmail.com>
2017-06-10 16:35:04 -0600

    added url

--- survey/views.py
+++ survey/views.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+
+def createSurvey(request):
+    return render(request, 'survey/create_survey.html')
