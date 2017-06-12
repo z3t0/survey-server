3713120ecd5878372971389fd58f7d67afd68de5
Rafi Khan <zetoslab@gmail.com>
2017-06-12 01:35:26 -0600

    missed commitd

--- survey/views.py
+++ survey/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 
 def createSurvey(request):
 
+    if request.method == "POST":
+        import pprint
+        pp = pprint.PrettyPrinter(indent = 4)
+        pp.pprint(request.POST)
 
     return render(request, 'survey/create_survey.html')
