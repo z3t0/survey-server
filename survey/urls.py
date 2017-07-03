c4599d5ff9fce6b47217a62c88bfaf101afb27e6
Rafi Khan <zetoslab@gmail.com>
2017-07-03 16:09:38 -0600

    Add survey action buttons and urls

--- survey/urls.py
+++ survey/urls.py
@@ -6,4 +6,7 @@ app_name = 'survey'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create-survey/$', views.createSurvey, name='create_survey'),
+    url(r'^(?P<survey_id>[0-9]+)/survey-edit/$', views.editSurvey, name='survey_edit'),
+    url(r'^(?P<survey_id>[0-9]+)/survey-fill/$', views.editSurvey, name='survey_fill'),
+    url(r'^(?P<survey_id>[0-9]+)/survey-results/$', views.editSurvey, name='survey_results'),
 ]
