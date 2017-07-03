3622fd3996e4bf44b52fdc46d74037579b8d8c6a
Rafi Khan <zetoslab@gmail.com>
2017-07-03 16:10:59 -0600

    Fixed views

--- survey/urls.py
+++ survey/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^create-survey/$', views.createSurvey, name='create_survey'),
     url(r'^(?P<survey_id>[0-9]+)/survey-edit/$', views.editSurvey, name='survey_edit'),
-    url(r'^(?P<survey_id>[0-9]+)/survey-fill/$', views.editSurvey, name='survey_fill'),
-    url(r'^(?P<survey_id>[0-9]+)/survey-results/$', views.editSurvey, name='survey_results'),
+    url(r'^(?P<survey_id>[0-9]+)/survey-fill/$', views.fillSurvey, name='survey_fill'),
+    url(r'^(?P<survey_id>[0-9]+)/survey-results/$', views.resultsSurvey, name='survey_results'),
 ]
