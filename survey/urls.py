538633f6467fd84d1c5494b07f464c6856ff65d4
Rafi Khan <zetoslab@gmail.com>
2017-07-03 03:23:24 -0600

    Modified survey urls
    '/' defaults to index page.

--- survey/urls.py
+++ survey/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'survey'
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^create-survey/$', views.createSurvey, name='create_survey'),
-    url(r'^index/$', views.index, name='index'),
 ]
