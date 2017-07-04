67c4066ead444babd4bf929c1699e6ae68fc1823
Rafi Khan <zetoslab@gmail.com>
2017-07-03 18:34:09 -0600

    Added get survey ajax
    Gets the survey data from the backend using ajax and the survey-data
    endpoint. Data on the backend is serialized with each model
    implementing a 'get_data' function.

--- bundles/fill/js/index.js
+++ bundles/fill/js/index.js
@@ -1,8 +1,27 @@
 import $ from "jquery";
+import {SurveyResponse} from './surveyResponse.js'
 
 $(document).ready(function() {
     // Submit data
     $('#btnSubmitAnswers').on('click', () => {
 	console.log("Submit data")
     })
+
+    // Get survey data
+    let data = {id: id}
+    $.ajax({
+	url: 'http://localhost:8000/survey-data/',
+	type: 'GET',
+	contentType: 'application/json; charset=utf-8',
+	data: data,
+	dataType: 'text',
+	success: (res) => {
+	    gotSurveyData(JSON.parse(res))
+	} 
+});
 })
+
+function gotSurveyData(data) {
+    console.log(data)
+
+}
